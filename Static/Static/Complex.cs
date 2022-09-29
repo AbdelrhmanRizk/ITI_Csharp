a3a0c7b5bf05a28cf8d5464c1ee86763720f285f
AbdelrhmanRizk <72870875+AbdelrhmanRizk@users.noreply.github.com>
2022-09-29 22:44:21 +0200

    Add files via upload

---
+++ Static/Static/Complex.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static
+{
+    class Complex
+    {
+        int real;
+        int img;
+        static int count;
+
+        public Complex(int real=0 , int img=0)
+        {
+            count++;
+            this.real = real;
+            this.img = img;
+        }
+        ~Complex(){
+            count--;
+        
+        }
+
+        public int Real { get => real; set => real = value; }
+        public int Img { get => img; set => img = value; }
+        public static int Count { get => count; set => count = value; }
+
+        public void print() {
+            Console.WriteLine($"{real}:{img}J");
+        
+        
+        }
+
+    }
+}
---
+++ Static/Static/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex C1 = new Complex(3,5);
+            Complex C2 = new Complex(5, 10);
+            Complex C3 = new Complex(15, 20);
+            C3.print();
+            Console.WriteLine(Complex.Count);
+            Console.ReadKey();
+
+        }
+    }
+}
