a990d59a1d24a5ab67c02bbbe66ada72c5b521fb
AbdelrhmanRizk <72870875+AbdelrhmanRizk@users.noreply.github.com>
2022-09-29 21:57:28 +0200

    Add files via upload

---
+++ Calculator/Calculator/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double val1 = Convert.ToDouble(textBox1.Text);
+            double val2 = Convert.ToDouble(textBox2.Text);
+            double sum = val1 + val2;
+            textBox3.Text = sum.ToString();
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double val1 = Convert.ToDouble(textBox1.Text);
+            double val2 = Convert.ToDouble(textBox2.Text);
+            double sub = val1 - val2;
+            textBox3.Text = sub.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double val1 = Convert.ToDouble(textBox1.Text);
+            double val2 = Convert.ToDouble(textBox2.Text);
+            double div = val1 / val2;
+            textBox3.Text = div.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double val1 = Convert.ToDouble(textBox1.Text);
+            double val2 = Convert.ToDouble(textBox2.Text);
+            double multi = val1 * val2;
+            textBox3.Text = multi.ToString();
+        }
+    }
+}
